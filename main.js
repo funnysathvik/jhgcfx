f3717e260d7f7fb61dfe274a4f1a5791335d3dfe
funnysathvik <130309025+funnysathvik@users.noreply.github.com>
2024-03-16 09:15:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ function preload(){
+  apple = loadImage(apple.png);
+  to_number = Number(content);
+  if(Number.isInteger(to_number))
+  screen_width = window.innerWidth;
+  screen_height = window.innerHeight;
+  createCanvas(screen_width,screen_height);
+ }
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
+speak();
